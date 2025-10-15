313bec498e0a8a9e66c5f82b8509de195db201ca
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2025-10-15 14:23:25 +0700

    'lan6'

--- client/src/components/myTask/MyTask_Modal.tsx
+++ client/src/components/myTask/MyTask_Modal.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../../redux/store/store';
-import { clearActionTask } from '../../redux/reducers/modalTask';
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../../redux/store/store";
+import { clearActionTask } from "../../redux/reducers/modalTask";
 
 export default function MyTask_Modal() {
-   const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
   return (
-     <div className="delete-container" id="delete-container">
-          <div className="delete">
-            <div className="delete-navbar">
-              <h3>Cập nhật trạng thái</h3>
-              <i className="fa-solid fa-xmark x exit" onClick={() => dispatch(clearActionTask())}></i>
-            </div>
-            <div className="delete-body">
-              <p>Xác nhận cập nhật trạng thái nhiệm vụ</p>
-            </div>
-            <div className="delete-footer">
-              <button className="btn-cancel" onClick={() => dispatch(clearActionTask())} >Huỷ </button>
-              <button className="btn-save" >
-                Xác nhận
-              </button>
-            </div>
-          </div>
+    <div className="delete-container" id="delete-container">
+      <div className="delete">
+        <div className="delete-navbar">
+          <h3>Cập nhật trạng thái</h3>
+          <i
+            className="fa-solid fa-xmark x exit"
+            onClick={() => dispatch(clearActionTask())}
+          ></i>
         </div>
-  )
+        <div className="delete-body">
+          <p>Xác nhận cập nhật trạng thái nhiệm vụ</p>
+        </div>
+        <div className="delete-footer">
+          <button
+            className="btn-cancel"
+            onClick={() => dispatch(clearActionTask())}
+          >
+            Huỷ{" "}
+          </button>
+          <button className="btn-save">Xác nhận</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
